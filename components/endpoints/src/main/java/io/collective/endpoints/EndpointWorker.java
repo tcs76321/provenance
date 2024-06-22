5d559cd5d5a6f7e9e0d3f55c462645fbae8d1cca
Trevor Stahl <trevor@trevorstahl.com>
2024-06-22 18:14:24 -0500

    complete EndpointWorker, endpoints test passes

--- components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
+++ components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
@@ -1,13 +1,18 @@
 package io.collective.endpoints;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import io.collective.articles.ArticleDataGateway;
+import io.collective.articles.ArticleInfo;
 import io.collective.restsupport.RestTemplate;
+import io.collective.rss.RSS;
 import io.collective.workflow.Worker;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EndpointWorker implements Worker<EndpointTask> {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -30,8 +35,15 @@ public class EndpointWorker implements Worker<EndpointTask> {
         String response = template.get(task.getEndpoint(), task.getAccept());
         gateway.clear();
 
-        { // todo - map rss results to an article infos collection and save articles infos to the article gateway
-
+        {
+            // map the response to rss object, from readme
+            RSS rss = new XmlMapper().readValue(response, RSS.class);
+            // map rss results to an article infos collection
+            List< ArticleInfo> articleInfos = rss.getChannel().getItem().stream()
+                    .map(item -> new ArticleInfo(0, item.getTitle()))
+                    .collect(Collectors.toList());
+            // save articles infos to the article gateway
+            articleInfos.forEach(info -> gateway.save(info.getTitle()));
         }
     }
 }
